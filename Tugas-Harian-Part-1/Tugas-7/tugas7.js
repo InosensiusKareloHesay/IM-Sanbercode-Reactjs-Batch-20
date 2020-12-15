b2c7ce76bc31bc4c8b109a625a90f752498b3924
Inosensius Karelo Hesay <inosensius241099@gmail.com>
2020-12-15 20:24:55 +0700

    Tugas-7

---
+++ Tugas-Harian-Part-1/Tugas-7/tugas7.js
@@ -0,0 +1,90 @@
+// Soal 1
+class Animal {
+    constructor(name){
+        this._name = name
+        this._legs = 4
+        this._cold_blooded = false
+    }
+
+    get name(){
+        return this._name
+    }
+
+    get legs(){
+        return this._legs
+    }
+
+    get cold_blooded(){
+        return this._cold_blooded
+    }
+}
+
+class Ape extends Animal{
+    constructor(name){
+        super(name)
+        this._legs = 2
+    }
+    yell(){
+        console.log("Auooo")
+    }
+}
+
+class Frog extends Animal{
+    constructor(name){
+        super(name)
+    }
+    jump(){
+        console.log("hop hop")
+    }
+}
+
+var sheep = new Animal("shaun");
+ 
+console.log(sheep.name) // "shaun"
+console.log(sheep.legs) // 4
+console.log(sheep.cold_blooded) // false
+
+var sungokong = new Ape("kera sakti")
+sungokong.yell() // "Auooo"
+ 
+var kodok = new Frog("buduk")
+kodok.jump() // "hop hop
+
+
+// Soal 2
+
+class Clock {
+    constructor(template){
+        this._template = template
+        this._timer = 0
+    }
+
+    start(){
+        this.render()
+        this._timer = setInterval(this.render, 1000)
+    }
+
+    stop(){
+        clearInterval(this._timer);
+    }
+
+    render(){
+        this._date = new Date()
+
+        this._hours = this._date.getHours();
+        if (this._hours < 10) this._hours = '0' + this._hours;
+  
+        this._mins = this._date.getMinutes();
+        if (this._mins < 10) this._mins = '0' + this._mins;
+  
+        this._secs = this._date.getSeconds();
+        if (this._secs < 10) this._secs = '0' + this._secs;
+  
+        this._output = `${this._hours}:${this._mins}:${this._secs}` 
+  
+        console.log(this._output);
+    }
+}
+  
+var clock = new Clock({template: 'h:m:s'});
+clock.start(); 
